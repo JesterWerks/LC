e17ec37028a46e7b5735a420cc8ef8f2d1c8fa7c
JesterWerks <sthorp@TPG145.TPG.com>
2015-08-19 16:45:39 -0500

    entity framework and domain added
    Added Entity Framework, Domain and added the LC database data objects.

---
+++ LottoContinuum/LottoContinuum.Domain/DomainModels/Drawing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoContinuum.Domain.DomainModels
+{
+    public class Drawing
+    {
+
+    }
+}
